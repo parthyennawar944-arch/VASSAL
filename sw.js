3170fb22347f0842962a509b4d3d557c86d2c0c4
parthyennawar944-arch <parthyennawar944@gmail.com>
2026-06-12 17:08:24 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+/**
+ * VASSAL Service Worker
+ * Ensures sovereign system resilience and offline capability.
+ */
+
+const CACHE_NAME = 'vassal-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'vassal_avatar.png'
+];
+
+// Install Event: Caching the core sovereign assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event: Serving assets from cache, falling back to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Event: Cleaning up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
